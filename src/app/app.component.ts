8ae349758829363921da13e659a455932a755e6e
Miguel <lowreybadboy@gmail.com>
2017-05-24 22:49:31 +0200

    primer commit de mi agenda

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+//seguir desde min 70 sesion 2 dia 1
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
