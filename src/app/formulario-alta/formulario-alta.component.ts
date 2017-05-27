ebfb87df2e7ff91788fe1bde7f0b2689e079c4bd
javivega <marribas44@gmail.com>
2017-05-27 08:42:46 +0200

    comienzo formulario de alta

--- src/app/formulario-alta/formulario-alta.component.ts
+++ src/app/formulario-alta/formulario-alta.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Contacto } from '../contacto';
 
 @Component({
   selector: 'formulario-alta',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioAltaComponent implements OnInit {
 
-  constructor() { }
+  nombre: string;
+  //Creo mi atributo que sera un evento que sera el que se emita luego mas tarde, para notificar.
+  @Output() alCrearContacto: EventEmitter<Contacto>;
+
+  constructor() {
+    this.alCrearContacto = new EventEmitter<Contacto>();
+  }
 
   ngOnInit() {
+  }
+
+  notificarCreaccionContacto(): void{
+    // Creo un nuevo objeto del tipo contacto a partir del nombre introducido en la caja de texto 
+    let contacto: Contacto = new Contacto(this.nombre);
+    this.alCrearContacto.emit(contacto);
   }
 
 }
